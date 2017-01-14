558b5297d94ed22d30dcd83673731947bdda50b5
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-14 12:40:03 -0800

    popup in react!

--- app/popup/src/scripts/index.jsx
+++ app/popup/src/scripts/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Store } from 'react-chrome-redux';
+import {render} from 'react-dom';
 
 import App from './components/app';
 
-const store = new Store({
-  state: {}
-});
-
 render(
-  <Provider store={store}>
     <App/>
-  </Provider>
-  , docuemnt.getElementById('app')
-);
+  , document.getElementById('app'));
