6ab371805419097a79531d842d26de6800b34d02
Edmund Li <edmund.xz.lee@gmail.com>
2017-01-15 10:57:09 -0800

    code style

--- app/popup/src/scripts/index.jsx
+++ app/popup/src/scripts/index.jsx
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 
 import App from './components/app';
 
-render(
-    <App/>
-  , document.getElementById('app'));
+render (
+  <App/>,
+  document.getElementById('app')
+);
