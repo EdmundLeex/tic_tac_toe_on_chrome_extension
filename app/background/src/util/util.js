3d1bdc0895796600e8aa978cc45b94b4a3eeded4
Edmund Li <edmund.xz.lee@gmail.com>
2017-04-02 09:24:00 -0700

    move base url out of it

--- app/background/src/util/util.js
+++ app/background/src/util/util.js
@@ -1,8 +1,5 @@
 import BASE_URL from '../../../base_url';
 
-// export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = 'https://super-tic-tac-toe-server.herokuapp.com';
-
 export const defaultHeaders = {
   'Access-Control-Allow-Origin': BASE_URL,
   'Accept': 'application/json',
