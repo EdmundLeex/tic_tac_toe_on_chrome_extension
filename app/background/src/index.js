888ab5f70dcaa51eb13699be6f406cbe52ea1ce1
Edmund Li <edmund.xz.lee@gmail.com>
2017-04-01 17:32:15 -0700

    updated redux logger

--- app/background/src/index.js
+++ app/background/src/index.js
@@ -1,13 +1,11 @@
 import reducer from './reducers/index';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import logger from 'redux-logger'
 import { alias, wrapStore } from 'react-chrome-redux';
 import { fetchGames } from './actions/index';
 
 import aliases from './aliases/index';
-
-const logger = createLogger();
 
 const enhancer = applyMiddleware(
   alias(aliases),
