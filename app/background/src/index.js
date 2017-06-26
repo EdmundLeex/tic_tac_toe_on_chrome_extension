8decc6918e3f264b41f35e8d483772d82744f7ef
Edmund Li <edmund.xz.lee@gmail.com>
2017-06-25 23:24:46 -0700

    not include logger if not in production

--- app/background/src/index.js
+++ app/background/src/index.js
@@ -1,28 +1,32 @@
 import reducer from './reducers/index';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
 import { alias, wrapStore } from 'react-chrome-redux';
 import { fetchGames } from './actions/index';
 
 import aliases from './aliases/index';
 
-const enhancer = applyMiddleware(
-  alias(aliases),
-  thunk,
-  logger
-);
+chrome.management.getSelf(app => {
+  let middlewares = [alias(aliases), thunk];
 
-const store = createStore(reducer, enhancer);
-
-window.setInterval(() => {
-  if (store.getState().appState.get('loggedIn') === true) {
-    store.dispatch(fetchGames());
-  } else {
-    // no op
+  if (app.installType === 'development') {
+    const { logger } = require('redux-logger');
+    middlewares.push(logger);
   }
-}, 5000);
 
-wrapStore(store, {
-  portName: 'SUPER_TTT'
+  const enhancer = applyMiddleware(...middlewares);
+  const store = createStore(reducer, enhancer);
+
+  window.setInterval(() => {
+    if (store.getState().appState.get('loggedIn') === true) {
+      store.dispatch(fetchGames());
+    } else {
+      // no op
+    }
+  }, 5000);
+
+  wrapStore(store, {
+    portName: 'SUPER_TTT'
+  });
 });
+
